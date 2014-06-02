4eba4262341c4cb60f7ea0b7aa0b4d6130651e06
oleh.kastornov <oleh.kastornov@masterofcode.com>
2014-06-02 17:51:36 +0300

    basic mockup, basic structure of client-side application

--- public/js/app.js
+++ public/js/app.js
@@ -6,5 +6,20 @@
         mapTypeId: google.maps.MapTypeId.ROADMAP
     },
     map = new google.maps.Map($('.map-container')[0], mapOptions);
+//    $('.active-edit').css('active-edit');
 
+    $('.btn-slide').on('click', function(event){
+        var slideContent = $(this).parent().find('.slide');
+        if(slideContent.hasClass('hidden')){
+            slideContent.removeClass('hidden')
+        }
+        else{
+            slideContent.addClass('hidden')
+        }
+    });
+
+    google.maps.event.addListener(map, 'click', function(event) {
+        $('#marker-latitude')[0].value = event.latLng.k;
+        $('#marker-longitude')[0].value = event.latLng.A;
+    });
 })(jQuery);
